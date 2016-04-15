4062138b56fa51fda64ec023132c924d2c7c9ba9
Mike Hoffert <mike.hoffert@gmail.com>
2016-04-15 13:51:21 -0600

    Production settings for heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :api, Api.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "rocketnanny.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -60,6 +61,8 @@ config :logger, level: :info
 #
 #     config :api, Api.Endpoint, root: "."
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+config :rocket_nanny, Api.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20,
+  ssl: false
