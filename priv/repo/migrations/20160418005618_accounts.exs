a802414fd48febe8451dfb8d8b785cbe8bc3f5fb
Mike Hoffert <mike.hoffert@gmail.com>
2016-04-17 19:31:37 -0600

    Update signup to create an account for the user

---
+++ priv/repo/migrations/20160418005618_accounts.exs
@@ -0,0 +1,15 @@
+defmodule Api.Repo.Migrations.Accounts do
+  use Ecto.Migration
+
+  def change do
+    create table(:accounts) do
+      timestamps
+    end
+
+    alter table(:users) do
+      add :account_id, references(:accounts)
+    end
+
+    create index(:users, [:account_id])
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,8 +1,13 @@
 defmodule Api.Factory do
   use ExMachina.Ecto, repo: Api.Repo
 
+  def factory(:account) do
+    %Api.Account{}
+  end
+
   def factory(:user) do
     %Api.User{
+      account_id: create(:account).id,
       first_name: sequence(:first_name, &"User#{&1}"),
       last_name: sequence(:last_name, &"User#{&1}"),
       email: sequence(:email, &"test#{&1}@test.com"),
