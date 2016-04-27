a79fcd153fc9f61ff752322ff0291c7de6ac87ce
Mike Hoffert <mike.hoffert@gmail.com>
2016-04-26 23:50:30 -0600

    v0.0.2 for deploy

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WhosAble.Mixfile do
 
   def project do
     [app: :whos_able,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
