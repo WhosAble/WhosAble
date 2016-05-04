d0203d9b836e9c1b4e43d87b725d1ba7d135f20e
huhwhatyah <huhwhatyah@gmail.com>
2016-05-04 18:21:39 -0400

    Bind state for service type

--- web/static/js/react/forms/jobtype.js
+++ web/static/js/react/forms/jobtype.js
@@ -1,10 +1,30 @@
+var TextField = require("./text-field");
 var JobType = React.createClass({
+  getInitialState() {
+    return {
+      servicetype: null,
+      errors: []
+    };
+  },
 handleCreate() {
   window.Dispatcher.createService(this.state.servicetype)
     .receive("ok", (resp) => {
       this.setState({serviceID: resp.service_id});
     }).receive("error", (resp) => {
       this.setState({errors: resp.errors});
+    });
+    this.props.onformchange()
+  },
+  handleFieldChange(field, val) {
+    var obj = {};
+    obj[field] = val;
+    this.setState(obj);
+  },
+  parseErrors(field) {
+    if(this.state.errors.length == 0) { return []; }
+
+    return this.state.errors.map(function(error) {
+      if(error.field == field) { return error; }
     });
   },
   render() {
@@ -13,11 +33,10 @@ handleCreate() {
 Service Type:
 <br/><br/>
 <form>
-
-<input type="text" placeholder="Example: Dinner Server"/>
+<TextField label="servicetype" value={ this.state.servicetype } errors={ this.parseErrors("servicetype") } onChange={ this.handleFieldChange.bind(this, "servicetype") }/>
 </form>
       <br/><br/>
-      <div className="btn btn-primary" onClick={this.props.onformchange}>Submit Service Type</div>
+      <div className="btn btn-primary" onClick={this.handleCreate}>Submit Service Type</div>
 </div>
     );
   }
