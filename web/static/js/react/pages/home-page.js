22685fb51d0bb70c298d70b876f5d7bbfbf0aa1a
Mike Hoffert <mike.hoffert@gmail.com>
2016-05-27 18:54:31 -0600

    Tweek homepage padding and color

--- web/static/js/react/pages/home-page.js
+++ web/static/js/react/pages/home-page.js
@@ -37,55 +37,57 @@ var HomePage = React.createClass({
         <NavBar isLoggedIn={ this.state.isLoggedIn }/>
         <div id="tophalf" className="container">
           <main role="main">
-            <div id="titlequote">"We're organizing services via SMS and email"
-              <br/><br/>
-              </div>
-              <div id="with">
-              With WhosAble, the painful process of identifying and scheduling available service providers around a task is made ridiculously simple.
+            <div className="titlequote">
+              <div>"We're organizing</div>
+              <div>services via</div>
+              <div>SMS and email"</div>
             </div>
-            <br/><br/>
+            <div id="with">
+              <div>With WhosAble, the painful process of identifying and scheduling available service providers around a task is made ridiculously simple.</div>
+            </div>
             <div id="buttons" className="row">
-            <div className="col-xs-6">
-            <div className="btn btn-primary">Watch the video</div>
+              <div className="col-xs-6">
+                <div className="btn btn-primary">Watch the video</div>
+              </div>
+              <div className="col-xs-6">
+                <Link to="signup">
+                  <div className="btn btn-primary">Signup</div>
+                </Link>
+              </div>
+              <hr/>
             </div>
-            <div className="col-xs-6">
-            <Link to="signup">
-            <div className="btn btn-primary">Signup</div>
-            </Link>
-            </div>
-            <hr/>
-            </div>
-            </main>
-            </div>
-
-          <div id="bottomhalf" className="container">
-            <main role="main">
-          <br/><br/>
-          <div id="platform">
-        WhosAble is a communication platform that handles notifications between you and your service provider and gets you from job need to a scheduled confirmation quickly and seamlessly.
-        </div>
-        <br/>
-<div id="icons" className="row">
-<div className="col-xs-4">
-<Link to="/secure">Secure
-<i className="fa fa-lock fa-3x"></i>
-</Link>
-</div>
-<div className="col-xs-4">
-<Link to="/connect">Connect to your employees
-<i className="fa fa-users fa-3x"></i>
-</Link>
-</div>
-<div className="col-xs-4">
-<Link to="/affordable">Affordable
-<i className="fa fa-usd fa-3x"></i>
-</Link>
-</div>
-</div>
-        </main>
-        </div>
+          </main>
         </div>
 
+        <div id="bottomhalf" className="container">
+          <main role="main">
+            <div id="platform">
+              WhosAble is a communication platform that handles notifications between you and your service provider and gets you from job need to a scheduled confirmation quickly and seamlessly.
+            </div>
+            <br/>
+            <div id="icons" className="row">
+              <div className="col-xs-12 col-md-4 icon secure-icon">
+                <Link to="/secure">
+                  <i className="fa fa-lock fa-3x fa-fw"></i>
+                  <div>Secure</div>
+                </Link>
+              </div>
+              <div className="col-xs-12 col-md-4 icon connect-icon">
+                <Link to="/connect">
+                  <i className="fa fa-users fa-3x fa-fw"></i>
+                  <div>Connect to your employees</div>
+                </Link>
+              </div>
+              <div className="col-xs-12 col-md-4 icon affordable-icon">
+                <Link to="/affordable">
+                  <i className="fa fa-usd fa-3x fa-fw"></i>
+                  <div>Affordable</div>
+                </Link>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
     );
   }
 });
