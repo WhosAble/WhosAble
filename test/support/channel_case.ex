0de994485efd1158e97b9b2a57a215f4bb04b63e
Mike Hoffert <mike.hoffert@gmail.com>
2016-07-05 08:01:30 -0600

    Upgrade to elixir 1.3.1

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -33,7 +33,7 @@ defmodule WhosAble.ChannelCase do
 
   setup tags do
     unless tags[:async] do
-      Ecto.Adapters.SQL.restart_test_transaction(WhosAble.Repo, [])
+      Ecto.Adapters.SQL.Sandbox.mode(WhosAble.Repo, {:shared, self()})
     end
 
     :ok
