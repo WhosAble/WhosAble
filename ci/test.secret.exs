3dd01f52662f95720e7403842445bd841001710d
Mike Hoffert <mike.hoffert@gmail.com>
2016-07-14 09:55:42 -0600

    Try a different way of copying config files for circle ci

--- ci/test.secret.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-# Configure your database
-config :whos_able, WhosAble.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  database: "whos_able_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
